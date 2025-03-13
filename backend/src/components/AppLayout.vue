d83eaa904de7b1da71661cbcf7cf1df8ab321f0e
Nathã Emanuel <92585791+NathaEmanuel17@users.noreply.github.com>
2025-03-13 19:19:59 -0300

    first commit

---
+++ backend/src/components/AppLayout.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="min-h-full bg-gray-200 flex">
+
+        <div class="w-[200px] bg-indigo-700">
+            <sidebar/>
+        </div>
+
+        <!-- Sidebar -->
+        <div class="flex-1">
+            <header class="h-8 shadow bg-white">
+                Header
+            </header>
+            <!-- Content -->
+
+            <main>
+                <router-view></router-view>
+            </main>
+        </div>
+        <!-- Content -->
+    </div>
+</template>
+
+
+<script setup>
+
+import Sidebar from "./Sidebar.vue";
+
+const {title} = defineProps({
+    title: String
+})
+</script>
+
+<style scoped>
+</style>
---
+++ backend/src/components/Sidebar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="w-[200px]  transition-all bg-indigo-700 text-white py-4 px-2">
+        <router-link :to="{name: 'app.dashboard'}" class="flex items-center p-2 rounded transition-colors hover:bg-black/30 font-light">
+            <span class="mr-2 text-gray-300">
+                <HomeIcon class="w-5"/>
+            </span>
+            <span class="text-xs">
+                Dashboard
+            </span>
+        </router-link>
+        <router-link :to="{name: 'app.dashboard'}" class="flex items-center p-2 rounded transition-colors hover:bg-black/30 font-light">
+            <span class="mr-2 text-gray-300">
+                <ListBulletIcon class="w-5"/>
+            </span>
+            <span class="text-xs">
+                Products
+            </span>
+        </router-link>
+        <router-link :to="{name: 'app.dashboard'}" class="flex items-center p-2 rounded transition-colors hover:bg-black/30 font-light">
+            <span class="mr-2 text-gray-300">
+                <UserIcon class="w-5"/>
+            </span>
+            <span class="text-xs">
+                Users
+            </span>
+        </router-link>
+        <router-link :to="{name: 'app.dashboard'}" class="flex items-center p-2 rounded transition-colors hover:bg-black/30 font-light">
+            <span class="mr-2 text-gray-300">
+                <ChartBarIcon class="w-5"/>
+            </span>
+            <span class="text-xs">
+                Reports
+            </span>
+        </router-link>
+    </div>
+</template>
+
+
+<script setup>
+import {HomeIcon, UserIcon, ListBulletIcon, ChartBarIcon} from '@heroicons/vue/24/outline/index.js'
+</script>
+
+<style scoped>
+</style>
